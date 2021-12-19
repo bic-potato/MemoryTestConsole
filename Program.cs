22ad2be8c4e5d94d4033c2c3e356b42fefa60e84
bic-potato <73098821+bic-potato@users.noreply.github.com>
2021-12-19 15:28:02 +0800

    feat: main feature

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Diagnostics;
+using System.Timers;
+
+int[] NumInput = new int[10];
+int[] NumOutput = new int[10];
+int Time = 60;
+System.Console.SetCursorPosition(0, 0);
+System.Console.WriteLine($"剩余时间：{Time}");
+System.Console.SetCursorPosition(0, 3);
+int rightNum = 0;
+for (int j = 0; j < NumOutput.Length; j++)
+{
+    while (true)
+    {
+        Random ran = new Random();
+        int num = ran.Next(10, 99);
+        if (Array.IndexOf(NumOutput, num) == -1)
+        {
+            NumOutput[j] = num;
+
+            Console.Write(NumOutput[j] +" ");
+            break;
+        }
+    }
+}
+Stopwatch stopwatch = null;
+while (true)
+{
+    if (stopwatch == null)
+    {
+        stopwatch = Stopwatch.StartNew();
+    }
+
+    // 时间到
+    if (stopwatch != null && stopwatch.Elapsed.TotalSeconds > Time)
+    {
+        stopwatch.Stop();
+        stopwatch = null;
+        break;
+
+    }
+    else
+    {
+        System.Console.SetCursorPosition(0, 0);
+
+        System.Console.WriteLine($"剩余时间：{Convert.ToInt32(Time - stopwatch.Elapsed.TotalSeconds):d2}");
+    }
+
+
+}
+
+Console.Clear();
+Console.Write("请输入数字，以\" \"分隔");
+
+
+string[] array1 = Console.ReadLine().Split();
+
+while (array1.Length > 10)
+{
+    Console.Write("你输入的数字个数大于10，请重新输入数字，以\" \"分隔");
+
+    array1 = Console.ReadLine().Split();
+}
+
+for(int i = 0; i< array1.Length; i++)
+{
+    NumInput[i] = Convert.ToInt32(array1[i]);
+
+}
+
+foreach (int num in NumInput)
+{
+    if (Array.IndexOf(NumOutput, num) != -1)
+    {
+        rightNum ++;
+    }
+}
+
+Console.Write("程序生成的数字为：");
+foreach (int num in NumOutput)
+{
+    Console.Write(num + " ");
+
+}
+Console.WriteLine();
+Console.Write("你输入的数字为：");
+foreach (int num in NumInput)
+{
+    Console.Write(num + " ");
+
+}
+Console.WriteLine();
+
+double rightNumfloat = Convert.ToDouble(rightNum);
+double rightNumdouble = Convert.ToDouble(10);
+
+Console.WriteLine($"正确数目：{rightNum} 错误数目{10 - rightNum}，正答率{rightNumfloat / rightNumdouble}");
+Console.Write("按任意键以退出");
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MemoryTestConsole
+
+基于 .Net 6 的命令行记忆测试系统
+
+## 测试原理
+
+每次测试会在屏幕上显示 10 个互不相同的2位数，持续 60 s，60s 后被测试者需要默写这些两位数，系统将判断默写的正确率。
+
+
+
+## 编译
+
+安装 Visual Studio 2022 和 .NET6 SDK，打开 MemoryTestConsoleApp.sln 文件，在 Visual Studio 2022 中编译运行.
